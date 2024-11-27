ce8d85555c5f47b9db4ac45c781daebf63f09e57
Julia Garant <jgarant10@gmail.com>
2024-11-27 16:20:12 -0500

    try catch and loading started

--- book-search/src/App.js
+++ book-search/src/App.js
@@ -1,53 +1,63 @@
 import './styles.css'
 
 let firstSearch = true;
+let errBlock = document.getElementById("errorBlock");
+
 
 function App() {
 
   function displayBooks(data){
+    let topWords = document.getElementById("top")
     let resultsOutput = document.getElementById("results")
-    if (firstSearch){
-      for (let i = 0; i < 10; i++) {
-        console.log(data.docs[i].cover_i, data.docs[i].title, data.docs[i].author_name, data.docs[i].first_publish_year); 
-  
-        let html = `<div id="book">`;
+    try{
+      
+      topWords.innerHTML = `<h2>Top 10 results:</h2> <hr/>`
+      if (firstSearch){
+        for (let i = 0; i < 10; i++) {
+          console.log(data.docs[i].cover_i, data.docs[i].title, data.docs[i].author_name, data.docs[i].first_publish_year); 
+    
+          let html = `<div id="book">`;
 
-        if (isNaN(data.docs[i].cover_i)) {
-          html += `<img id="bookCover" src="/imgs/cover-not-found.png" alt="Cover not found" />`;
-        } else {
-          html += `<img id="bookCover" src="https://covers.openlibrary.org/b/id/${data.docs[i].cover_i}-M.jpg" />`;
+          if (isNaN(data.docs[i].cover_i)) {
+            html += `<img id="bookCover" src="/imgs/cover-not-found.png" alt="Cover not found" />`;
+          } else {
+            html += `<img id="bookCover" src="https://covers.openlibrary.org/b/id/${data.docs[i].cover_i}-M.jpg" />`;
+          }
+
+          html += `
+            <h3>${data.docs[i].title}</h3>
+            <i>${data.docs[i].author_name[0]},</i>
+            First published in ${data.docs[i].first_publish_year}
+            </div>`;
+
+          resultsOutput.innerHTML += html;
+          firstSearch = false;
         }
+      } else {
+        resultsOutput.innerHTML = " "
+        for (let i = 0; i < 10; i++) {
+          console.log(data.docs[i].cover_i, data.docs[i].title, data.docs[i].author_name, data.docs[i].first_publish_year); 
 
-        html += `
-          <h2>${data.docs[i].title}</h2>
-          <i>${data.docs[i].author_name[0]},</i>
-          First published in ${data.docs[i].first_publish_year}
-        </div>`;
+          let html = `<div id="book">`;
+          
+          if (isNaN(data.docs[i].cover_i)) {
+            html += `<img id="bookCover" src="/imgs/cover-not-found.png" alt="Cover not found" />`;
+          } else {
+            html += `<img id="bookCover" src="https://covers.openlibrary.org/b/id/${data.docs[i].cover_i}-M.jpg" />`;
+          }
 
-        resultsOutput.innerHTML += html;
-        firstSearch = false;
-      }
-    } else {
-      resultsOutput.innerHTML = " "
-      for (let i = 0; i < 10; i++) {
-        console.log(data.docs[i].cover_i, data.docs[i].title, data.docs[i].author_name, data.docs[i].first_publish_year); 
+          html += `
+            <h2>${data.docs[i].title}</h2>
+            <i>${data.docs[i].author_name[0]},</i>
+            First published in ${data.docs[i].first_publish_year}
+          </div>`;
 
-        let html = `<div id="book">`;
-        
-        if (isNaN(data.docs[i].cover_i)) {
-          html += `<img id="bookCover" src="/imgs/cover-not-found.png" alt="Cover not found" />`;
-        } else {
-          html += `<img id="bookCover" src="https://covers.openlibrary.org/b/id/${data.docs[i].cover_i}-M.jpg" />`;
+          resultsOutput.innerHTML += html;
         }
-
-        html += `
-          <h2>${data.docs[i].title}</h2>
-          <i>${data.docs[i].author_name[0]},</i>
-          First published in ${data.docs[i].first_publish_year}
-        </div>`;
-
-        resultsOutput.innerHTML += html;
       }
+    }
+    catch(err){
+      errBlock.innerHTML = `Error loading books: ${err}`
     }
   }
 
@@ -58,13 +68,18 @@ function App() {
     for CORS error that appears on local testing.
     "Cross-Origin Request Blocked", "NetworkError when attempting to fetch resource."
     */ 
-    let searchInput = document.getElementById("search");
+    let searchInput = document.getElementById("search");    
 
-    // fetch('https://openlibrary.org//search.json?q='+ searchInput.value)
-    fetch('https://cors-anywhere.herokuapp.com/https://openlibrary.org//search.json?q='+ searchInput.value)
-    .then(response => response.json())
-    .then(data => displayBooks(data));
-    console.log("search book")
+    try{
+      // fetch('https://openlibrary.org//search.json?q='+ searchInput.value)
+      fetch('https://cors-anywhere.herokuapp.com/https://openlibrary.org//search.json?q='+ searchInput.value)
+      .then(response => response.json())
+      .then(data => displayBooks(data));
+      console.log("search book")
+    }
+    catch(err){
+      errBlock.innerHTML = `Error searching for book: ${err}`
+    }
   }
 
   return (
@@ -78,10 +93,12 @@ function App() {
       </div>
       <br/>
       <p id="errorBlock"></p>
+      
       <div id="background">
+        <div id="loading">
+          <img src="/imgs/SpinningBook.gif" />
+        </div>
         <div id="top">
-          <p>Top 10 results:</p>
-          <hr/>
         </div>
         <div id="results">
 
