fee5670f7c9c7ef2b7a1516b6b801feb9495fe7f
Julia Garant <jgarant10@gmail.com>
2024-11-27 15:13:00 -0500

    if no cover id, and if second search added

--- book-search/src/App.js
+++ book-search/src/App.js
@@ -1,41 +1,68 @@
 import './styles.css'
-/*
-Using 
-  https://cors-anywhere.herokuapp.com/corsdemo
-for CORS error that appears on local testing.
-"Cross-Origin Request Blocked", "NetworkError when attempting to fetch resource."
-*/ 
 
+let firstSearch = true;
 
 function App() {
 
   function displayBooks(data){
     let resultsOutput = document.getElementById("results")
+    if (firstSearch){
+      for (let i = 0; i < 10; i++) {
+        console.log(data.docs[i].cover_i, data.docs[i].title, data.docs[i].author_name, data.docs[i].first_publish_year); 
+  
+        let html = `<div id="book">`;
 
-    for (let i = 0; i < 10; i++) {
-      console.log(data.docs[i].cover_i, data.docs[i].title, data.docs[i].author_name, data.docs[i].first_publish_year); 
+        if (isNaN(data.docs[i].cover_i)) {
+          html += `<img id="bookCover" src="/imgs/cover-not-found.png" alt="Cover not found" />`;
+        } else {
+          html += `<img id="bookCover" src="https://covers.openlibrary.org/b/id/${data.docs[i].cover_i}-M.jpg" />`;
+        }
 
-      resultsOutput.innerHTML += 
-        `<div id="book"> 
-          <img id="bookCover" src="https://covers.openlibrary.org/b/id/${data.docs[i].cover_i}-M.jpg" />
-          <h2> ${data.docs[i].title} </h2>
-          <br/>
-          <i>${data.docs[i].author_name},</i>
-          First published in ${data.docs[i].first_publish_year}  
-        </div>`
+        html += `
+          <h2>${data.docs[i].title}</h2>
+          <i>${data.docs[i].author_name[0]},</i>
+          First published in ${data.docs[i].first_publish_year}
+        </div>`;
 
+        resultsOutput.innerHTML += html;
+        firstSearch = false;
+      }
+    } else {
+      resultsOutput.innerHTML = " "
+      for (let i = 0; i < 10; i++) {
+        console.log(data.docs[i].cover_i, data.docs[i].title, data.docs[i].author_name, data.docs[i].first_publish_year); 
+
+        let html = `<div id="book">`;
+        
+        if (isNaN(data.docs[i].cover_i)) {
+          html += `<img id="bookCover" src="/imgs/cover-not-found.png" alt="Cover not found" />`;
+        } else {
+          html += `<img id="bookCover" src="https://covers.openlibrary.org/b/id/${data.docs[i].cover_i}-M.jpg" />`;
+        }
+
+        html += `
+          <h2>${data.docs[i].title}</h2>
+          <i>${data.docs[i].author_name[0]},</i>
+          First published in ${data.docs[i].first_publish_year}
+        </div>`;
+
+        resultsOutput.innerHTML += html;
+      }
     }
   }
 
   function searchBook(){
+    /*
+    Using 
+      https://cors-anywhere.herokuapp.com/corsdemo
+    for CORS error that appears on local testing.
+    "Cross-Origin Request Blocked", "NetworkError when attempting to fetch resource."
+    */ 
     let searchInput = document.getElementById("search");
-    {console.log("searchInput: ")}
-    {console.log(searchInput)}
 
     // fetch('https://openlibrary.org//search.json?q='+ searchInput.value)
     fetch('https://cors-anywhere.herokuapp.com/https://openlibrary.org//search.json?q='+ searchInput.value)
     .then(response => response.json())
-    // .then(data => console.log(data));
     .then(data => displayBooks(data));
     console.log("search book")
   }
@@ -48,11 +75,17 @@ function App() {
       <div>
         <input id="search" type="text" placeholder='Search by title, author, etc.'/>
         <button onClick={searchBook} id="btnSearch">Search</button>
-        {/* <button onClick={handleClick} id="btnSearch">Search</button> */}
       </div>
       <br/>
-      <div id="results">
-        <p>Top 10 results:</p>
+      <p id="errorBlock"></p>
+      <div id="background">
+        <div id="top">
+          <p>Top 10 results:</p>
+          <hr/>
+        </div>
+        <div id="results">
+
+        </div>
       </div>
     </div>
   );
